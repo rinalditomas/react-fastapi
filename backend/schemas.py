253b79d26aab301f2e19de79960c7c7aab7af16f
rinalditomas <rinalditomas@gmail.com>
2024-06-23 09:19:37 +0200

    chore: Update User and Connection models and add UUID primary keys

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,24 +1,38 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from datetime import datetime
+from typing import List, Optional
+from uuid import UUID  # Import UUID
 
 class UserBase(BaseModel):
     name: str
+    email: str  # Changed from EmailStr to str
 
 class UserCreate(UserBase):
     pass
 
-class User(UserBase):
-    id: int
-    class Config:
-        from_attributes = True 
-
 class ConnectionBase(BaseModel):
-    user_id: int
-    connected_user_id: int
+    user_id: UUID
+    connected_user_id: UUID
 
 class ConnectionCreate(ConnectionBase):
     pass
 
+
 class Connection(ConnectionBase):
-    id: int
+    id: UUID
+    connection_made_at: datetime
     class Config:
-        from_attributes = True  
+        from_attributes = True
+
+class User(UserBase):
+    id: UUID
+    created_at: Optional[datetime] = None  # Make optional and default to None
+    last_updated_at: Optional[datetime] = None  # Make optional and default to None
+    class Config:
+        from_attributes = True
+
+class Stats(BaseModel):
+    user_count: int
+    connection_count: int
+    average_connections_per_user: float
+    last_connection: Optional[datetime] = None  
